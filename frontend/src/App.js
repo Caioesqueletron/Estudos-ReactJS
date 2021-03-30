53b484b1dbba9dcc03cfdf36a86e26458856e5dd
caioesqueletron <caiocaio620@gmail.com>
2021-03-30 17:25:08 -0300

    updating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import './App.css'
 import Header from './components/Header'
+import api from './services/api'
 
-function App(){
-    const projects = ['Desenvolvimento de app', 'Front-end web']
-    return (
-    <div>
-        
-        <Header title="Homepage"/>
-         <ul>
+function App() {
+    const [projects, setProjects] = useState([])
+    //useState retorna um array com 2 posições
+    //
+    //1.Variavel com o seu valor inicial
+    //2.Função para atualizarmos esse valor
 
-        </ul>
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data)
+        })
+    },[])
+
+    async function handleAddProject() {
+       // setProjects([...projects, `Novo projeto ${Date.now()}`])
+        //projects.push(`Novo projeto ${Date.now()}`)
         
-    </div>
-  
+      const response =  await api.post('projects',{
+            title:`Novo projeto ${Date.now()}`,
+            owner: "Caio"
+        })
+
+        const project = response.data;
+
+        setProjects([...projects,project])
     
+    }
+
+    return (
+        <div>
+
+            <Header title="Homepage" />
+            <ul>
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
+            </ul>
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+        </div>
+
+
     )
 }
 
