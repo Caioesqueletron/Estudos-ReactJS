61f9dd0a73077fa6fb1b982b7498853d6190e319
caioesqueletron <caiocaio620@gmail.com>
2021-03-30 02:19:15 -0300

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './components/Header'
+
+function App(){
+    const projects = ['Desenvolvimento de app', 'Front-end web']
+    return (
+    <div>
+        
+        <Header title="Homepage"/>
+         <ul>
+
+        </ul>
+        
+    </div>
+  
+    
+    )
+}
+
+export default App;
